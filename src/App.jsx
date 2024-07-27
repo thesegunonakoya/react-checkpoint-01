6ed544a54b88c553a7454217d6cb78cf5332ae22
thesegunonakoya <thesegunonakoya@gmail.com>
2024-07-27 18:42:06 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Card } from 'react-bootstrap';
+import Name from './components/Name';
+import Price from './components/Price';
+import Description from './components/Description';
+import Image from './components/Image';
+
+function App() {
+  const firstName = "Oluwasegun"; 
+
+  return (
+    <div className="container mt-5">
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Image />
+          <Card.Title><Name /></Card.Title>
+          <Card.Text><Price /></Card.Text>
+          <Card.Text><Description /></Card.Text>
+        </Card.Body>
+      </Card>
+      <p>
+        {firstName ? `Hello, ${firstName}` : "Hello, there!"}
+      </p>
+      {firstName && <img src="https://via.placeholder.com/100" alt="Profile" />}
+    </div>
+  );
+}
+
+export default App;
